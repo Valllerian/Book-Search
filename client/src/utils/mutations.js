463f3735db810a7cea4e838bb4e08529da5a3c1b
Valerii Bihun <valeriibihun.co@gmail.com>
2022-02-25 15:42:06 -0700

    added utils for the front end

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,28 @@
+import { gql } from '@apollo/client';
+
+// LOGIN USER mutation
+
+export const LOGIN_USER = gql`
+    mutation login($email: String!, $password: String!) {
+        login(email: $email, password: $password) {
+            token
+            user {
+                _id
+                username
+            }
+        }}
+`;
+
+// CREATE USER mutation;
+
+export const ADD_USER = gql`
+    mutation createUser($username: String!, $email: String!, $password: String!) {
+        createUser(username: $username, email: $email, password: $password) {
+            token
+            user {
+                _id
+                username
+            }
+        }
+    }
+`;
