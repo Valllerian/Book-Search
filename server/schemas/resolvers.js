1785c5cfacc189f47ec9d1275876f4909c17e0f7
Valerii Bihun <valeriibihun.co@gmail.com>
2022-02-25 15:34:44 -0700

    Added schemas  for the user

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,3 +1,36 @@
 const { User } = require('../models');
 
+const { AuthenticationError } = require('apollo-server-express');
+const { signToken } = require('../utils/auth');
+
+
+const resolvers = {
+
+    // getMe that returns only info about current user
+    Query: {
+        getMe: async (parent, args, context) => {
+            if (context.user) {
+              try {
+                const userData = await User.findOne({ _id: context.user._id }).select(
+                  "-__v -password"
+                );
+                return userData;
+              } catch (err) {
+                console.log(err);
+              }
+            }
+            throw new AuthenticationError("You need to be logged in!");
+          },
+        },
+
+        Mutation: {
+            createUser: async (parent, args) => {
+                const user = await User.create(args);
+                const token = signToken(user);
+                return { token, user };
+            },
+        }
+};
+
+
 module.exports = resolvers;
